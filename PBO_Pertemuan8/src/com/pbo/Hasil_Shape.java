9d6e402f01718aa39d1bb85e902b20dd92a409d3
AnjaysnitaSatriaIbanezia <71379388+AnjaysnitaSatriaIbanezia@users.noreply.github.com>
2020-11-16 00:09:48 +0700

    Add files via upload

---
+++ PBO_Pertemuan8/src/com/pbo/Hasil_Shape.java
@@ -0,0 +1,8 @@
+package com.pbo;
+
+public class Hasil_Shape {
+    public static void main(String[] args){
+        Triangle a = new Triangle(10,30);
+        System.out.println(a.getArea());
+    }
+}
